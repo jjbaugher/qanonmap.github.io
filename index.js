a0bbefeced984a5b745cfa82035144032e355815
|封量子⟩+|qanonmap⟩ <qntmpkts+qanonmap@protonmail.com>
2018-02-11 17:21:05 +0000

    Update index.js - fix 8ch broken links

--- index.js
+++ index.js
@@ -342,7 +342,7 @@ const legendPattern = new RegExp(`(?!<[a|abbr|strong][^>]*>)([^a-zA-Z]|!\D|!\w|^
 function addHighlights(text) {
     return !text
         ? ''
-        : text.replace(/(^>[^>].*\n?)+/g, (match) => `<q>${match}</q>`).replace(/(https?:\/\/[.\w\/?\-=&#]+)/g, (match) => match.endsWith('.jpg')
+        : text.replace(/(^>[^>].*\n?)+/g, (match) => `<q>${match}</q>`).replace(/https\:\/\/\s/g, (match) => `https://`).replace(/(https?:\/\/[.\w\/?\-=&#]+)/g, (match) => match.endsWith('.jpg')
             ? `<img src="${match}" alt="image">`
             : `<a href="${match}" target="_blank">${match}</a>`).replace(/(?<!<[^>]*)\/(?<!\n)/g, (match) => `&#47;`).replace(/(\[[^[]+])/g, (match) => `<strong>${match}</strong>`).replace(legendPattern, (match, p1, p2, p3, o, s) => `${p1}<abbr title="${legend[p2]}">${p2}</abbr>${p3}`);
 }
