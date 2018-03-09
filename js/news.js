fdde8c97e122796b45766ca641adf7b9544f8e6a
|封量子⟩+|qanonmap⟩ <qntmpkts+qanonmap@protonmail.com>
2018-03-09 02:37:16 +0000

    Add core.js code to news.js

--- js/news.js
+++ js/news.js
@@ -1,23 +1,159 @@
+let debug = window.location.hostname === 'localhost';
+
+const pipe = (...funcs) => i => funcs.reduce((p, c) => c(p), i);
+const create = (item, ...funcs) => funcs.reduce((p, c) => c(p), item);
+
+const getJson = url => fetch(url).then(response => response.json());
+const getLocalJson = filename => fetch(`data/json/${filename}.json`, {credentials: 'same-origin'}).then(r => r.json());
+const getHostname = urlString => new URL(urlString).hostname;
+const postJson = (url, object) => fetch(url, {
+    method: "post",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(object)
+});
+
+const forAll = (items, htmlCallback) => items && items instanceof Array
+    ? items
+        .map(htmlCallback)
+        .join('')
+    : '';
+const ifExists = (item, htmlCallback) => item
+    ? htmlCallback(item)
+    : '';
+
+const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+];
+const xx = x => (x < 10
+    ? '0'
+    : '') + x;
+const formatDate = date => `${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+const formatTime = date => `${xx(date.getHours())}:${xx(date.getMinutes())}:${xx(date.getSeconds())}`;
+
+const tag = (name, attributes) => {
+    const element = document.createElement(name);
+    if (attributes) {
+        for (const attribute of Object.keys(attributes)) {
+            element.setAttribute(attribute, attributes[attribute]);
+        }
+    }
+    return element;
+};
+tag.fromString = string => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = string;
+    return wrapper.firstElementChild;
+};
+const appendTo = container => element => container.appendChild(element);
+function bindRadios(name, form) {
+    const radios = form.querySelectorAll(`[name=${name}]`);
+    const panels = Array
+        .from(radios)
+        .map(radio => form.querySelector(`#details_${radio.value}`));
+    for (const radio of radios) {
+        radio.onchange = () => {
+            panels.forEach(p => {
+                const hide = p.id !== `details_${radio.value}`;
+                p.hidden = hide;
+                p
+                    .querySelectorAll('input,textarea')
+                    .forEach(i => i.disabled = hide);
+            });
+        };
+    }
+    radios
+        .item(0)
+        .checked = true;
+    radios
+        .item(0)
+        .onchange();
+}
+function Submission(form) {
+    const inputs = Array
+        .from(form.elements)
+        .filter(el => el.validity.valid && el.value !== '' && (el.type !== 'radio' || el.checked));
+    const submission = {};
+    for (const input of inputs) {
+        submission[input.name] = input.value;
+    }
+    return submission;
+}
+const onSubmit = form_event => form => {
+    form.onsubmit = form_event(form);
+    return form;
+};
+const getParams = query => {
+    if (!query) {
+        return {};
+    }
+
+    return (/^[?#]/.test(query)
+        ? query.slice(1)
+        : query)
+        .split('&')
+        .reduce((params, param) => {
+            let [key,
+                value] = param.split('=');
+            params[key] = value
+                ? decodeURIComponent(value
+                // .replace(/\+/g, ' ')
+                )
+                : '';
+            return params;
+        }, {});
+};
+const setParams = params => {
+    if (Object.keys(params).length) {
+        const value = Object
+            .keys(params)
+            .map(k => `${k}=${encodeURIComponent(params[k]
+            // .replace(/ /g, '+')
+            )}`)
+            .join('&');
+
+        history.replaceState({}, null, `?${value}`);
+    } else {
+        history.replaceState({}, null, `?`);
+    }
+};
+
 let news = [];
 function main() {
-    Promise.all([
-        getLocalJson('news'),
-    ]).then(values => {
-        news = values[0];
-        news.sort((a, b) => b.date - a.date);
-        initNews(news);
-        initSearch(news, document.querySelector('input[type=search]'));
-    });
+    Promise
+        .all([getLocalJson('news')])
+        .then(values => {
+            news = values[0];
+            news.sort((a, b) => b.date - a.date);
+            initNews(news);
+            initSearch(news, document.querySelector('input[type=search]'));
+        });
 }
 
 function initSearch(items, searchElement) {
     searchElement.oninput = () => {
-        const terms = searchElement.value.toLowerCase().split(' ');
+        const terms = searchElement
+            .value
+            .toLowerCase()
+            .split(' ');
 
-        const keywordInText = (term, text) => text.toLowerCase().includes(term);
-        const itemContains = item => term => (item.headline && keywordInText(term, item.headline))
-            || (item.description && keywordInText(term, item.description))
-            || keywordInText(term, item.url);
+        const keywordInText = (term, text) => text
+            .toLowerCase()
+            .includes(term);
+        const itemContains = item => term => (item.headline && keywordInText(term, item.headline)) || (item.description && keywordInText(term, item.description)) || keywordInText(term, item.url);
 
         const ids = items
             .filter(item => terms.every(itemContains(item)))
@@ -25,27 +161,32 @@ function initSearch(items, searchElement) {
 
         applyFilter(ids);
 
-        if(value === '')
+        if (value === '') 
             setParams({});
-        else
+        else 
             setParams({q: value});
-    };
+        }
+    ;
     const result = {};
     for (const item of items) {
         const hostname = getHostname(item.url);
-        if (!result[hostname]) result[hostname] = new Set();
+        if (!result[hostname]) 
+            result[hostname] = new Set();
         result[hostname].add(item.id);
     }
-    const resultList = Object.keys(result)
+    const resultList = Object
+        .keys(result)
         .map(k => ({line: k, ids: result[k]}))
         .filter(a => a.ids.size > 2);
 
     resultList.sort((a, b) => b.ids.size - a.ids.size);
     const datalist = document.querySelector('#hints');
-    datalist.innerHTML = resultList.map(i => `<option label="${i.ids.size}">${i.line}</option>`).join('\n');
+    datalist.innerHTML = resultList
+        .map(i => `<option label="${i.ids.size}">${i.line}</option>`)
+        .join('\n');
 
     const query = getParams(location.search);
-    if('q' in query) {
+    if ('q' in query) {
         searchElement.value = query.q;
         searchElement.oninput();
     }
@@ -81,16 +222,21 @@ function applyFilter(ids) {
             element.hidden = true;
         }
     }
-    for(const h3 of Array.from(document.querySelectorAll('.sticky'))) {
+    for (const h3 of Array.from(document.querySelectorAll('.sticky'))) {
         const section = h3.nextElementSibling;
-        h3.hidden = Array.from(section.children).every(c => c.hidden);
+        h3.hidden = Array
+            .from(section.children)
+            .every(c => c.hidden);
     }
-    document.querySelector('#count').textContent = `${count}`;
+    document
+        .querySelector('#count')
+        .textContent = `${count}`;
 }
 
 const html = {
     img: (src) => {
-        if (!src) return '';
+        if (!src) 
+            return '';
         return `<img src="${src}" class="contain" width="100" height="100">`;
     },
     date: item => {
@@ -113,7 +259,7 @@ const html = {
           </a>
         <small>${getHostname(item.url)}</small>
         </article>`;
-    },
+    }
 };
 
 document.addEventListener('DOMContentLoaded', main, false);
